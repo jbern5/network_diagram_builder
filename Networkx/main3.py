e4806d661a4c60718e567f4297594737b4f7683c
Justin Bernstein <justinbernstein5@gmail.com>
2020-03-22 14:03:06 -0400

    fixed spacing bug

--- Networkx/main3.py
+++ Networkx/main3.py
@@ -32,35 +32,39 @@ y_pos3 = 300
 #             if node['data_center'] == k:
 #                 v.append(node['ip'])
 
+# Gets each data center and creates a group for each unique data center
 for dc in df['data_center'].unique():
     if str(dc) != 'nan':
         data['data_centers'].append(g.add_group(str(dc)))
 
+
 for index, node in df.iterrows():
     if node['zone'] in level1:
-        level1_count += 1
+        data['level1_count'] += 1
     elif node['zone'] in level2:
-        level2_count += 1
+        data['level2_count'] += 1
     elif node['zone'] in level3:
-        level3_count += 1
-print(data['data_centers'][0].label)
+        data['level3_count'] += 1
+
 # Nodes
 for index, node in df.iterrows():
     for dc in data['data_centers']:
         if node['zone'] in level1:
-            x_pos1 += (MAX_WIDTH / (level1_count + 1))
+            print(data['level1_count'])
+
             if node['data_center'] == dc.label:
+                x_pos1 += (MAX_WIDTH / (data['level1_count'] + 1))
                 dc.add_node(node['ip'], label=str(node['ip'] + "\n" + node['version']), height="50", width="100", x=str(x_pos1), y=str(y_pos1))
+            # else:
+            #     g.add_node(node['ip'], label=str(node['ip'] + "\n" + node['version']), height="50", width="100", x=str(x_pos1), y=str(y_pos1))
         if node['zone'] in level2:
-            x_pos2 += (MAX_WIDTH / (level2_count + 1))
             if node['data_center'] == dc.label:
+                x_pos2 += (MAX_WIDTH / (data['level2_count'] + 1))
                 dc.add_node(node['ip'], label=str(node['ip'] + "\n" + node['version']), height="50", width="100", x=str(x_pos2), y=str(y_pos2))
         if node['zone'] in level3:
-            x_pos3 += (MAX_WIDTH / level3_count)
             if node['data_center'] == dc.label:
+                x_pos3 += (MAX_WIDTH / data['level3_count'])
                 dc.add_node(node['ip'], label=str(node['ip'] + "\n" + node['version']), height="50", width="100", x=str(x_pos3), y=str(y_pos3))
-
-
 
 # Edges
 for index, node in df.iterrows():
@@ -72,13 +76,11 @@ for index, node in df.iterrows():
 for conn in listOfConnections:
     g.add_edge(conn[0], conn[1])
 
-
-
 z = g.get_graph()
 
-# print(z)
+graph_file = open("test3" + '.graphml', 'w')
+graph_file.write(z)
+graph_file.close()
 
 
-new_config_file = open("test1" +'.graphml','w')
-new_config_file.write(z)
-new_config_file.close()
+# data center object that takes a list of child node objects
